bce0ded0eb1b3f5bb0bf29db1a43b1a92f11b948
Keungeun LEE <klee8903@gmail.com>
2022-09-13 21:02:37 +0200

    js00/ex03 ing

---
+++ js00/ex03/dynamicType.js
@@ -0,0 +1,24 @@
+// const dynamicType = {
+//     ...
+//     put : ...
+//     change : ...
+//     printType : ...
+// }
+
+const type = {
+	put: function(typeof Number) {
+
+	}
+}
+
+put (typeof Number) {
+
+}
+
+change() {
+
+}
+
+printType () {
+
+}
