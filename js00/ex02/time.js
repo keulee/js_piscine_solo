d9adad9c291bb4770db152c9a3afc146977c487c
Keungeun LEE <klee8903@gmail.com>
2022-09-13 20:34:58 +0200

    js00/ex02 done

---
+++ js00/ex02/time.js
@@ -0,0 +1,17 @@
+const month = 7;
+let day = 13;
+
+const timeWarp = (newMonth, newDay) => {
+  // TODO
+  if (month === 7) {
+	let month = newMonth;
+	console.log(month + " " + newDay);
+	// console.log(newDay);
+	}
+}
+
+console.log(month + " " + day);
+
+timeWarp(5, 10);
+timeWarp(11, 20);
+
