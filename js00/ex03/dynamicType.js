d93751bad1db6f3f5ccddece904ed03fe2def69b
keungeun Lee <klee8903@gmail.com>
2022-09-20 23:05:43 +0200

    JS00/ex03 ing

--- js00/ex03/dynamicType.js
+++ js00/ex03/dynamicType.js
@@ -1,10 +1,25 @@
-// const dynamicType = {
-//     ...
-//     put : ...
-//     change : ...
-//     printType : ...
-// }
-
-put (typeof i === "number") {
-
+const dynamicType = {
+    val : undefined,
+    put : function(input) {
+        if (typeof input == 'number')
+            this.val = input;
+        else
+            console.log("Put number value plz");
+    },
+    change : function(input) {
+        if (input === "string")
+            // console.log("here");
+            this.val = String(this.val);
+    },
+    printType : function(val) {
+        console.log(typeof val);
+    }
 }
+
+
+let test1 = dynamicType;
+console.log(test1.val);
+test1.put(42);
+console.log(test1.val);
+test1.change("string");
+test1.printType(test1.val);
