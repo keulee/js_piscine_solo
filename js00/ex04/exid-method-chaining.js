17538e58633f7d33c83c510b5784c223bcb79c71
Keungeun LEE <klee8903@gmail.com>
2022-09-21 10:23:36 +0200

    js00/ex04 done / js_note added

---
+++ js00/ex04/exid-method-chaining.js
@@ -0,0 +1,19 @@
+const exidMethodChaining = {
+	count : 10,
+	up : function() {
+		this.count++;
+		return (this);
+	},
+	down : function() {
+		this.count--;
+		return(this);
+	},
+	showCount : function() {
+		console.log(this.count);
+		return(this);
+	}
+};
+
+
+
+exidMethodChaining.up().down().up().up().down().up().down().up().up().down().showCount()
