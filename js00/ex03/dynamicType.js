18d9fd945951f0d338f6ad388c2cd43a70dde1a8
Keungeun LEE <klee8903@gmail.com>
2022-09-21 10:05:25 +0200

    JS00/ex03 done

--- js00/ex03/dynamicType.js
+++ js00/ex03/dynamicType.js
@@ -7,19 +7,59 @@ const dynamicType = {
             console.log("Put number value plz");
     },
     change : function(input) {
-        if (input === "string")
+        if (input === "String")
             // console.log("here");
             this.val = String(this.val);
+			// this.val = Number(this.val).toString(this.val);
+			// null, undefined는 toString()으로 변환 불가, String()은 가능.
+		else if (input == "Number")
+			this.val = Number(this.val);
+		else if (input == "Object")
+			// this.val = Object(this.val); //?
+			// this.val = { put: this.val };
+			this.val = new Number(this.val);
+		else if (input == "Array")
+			this.val = [this.val];
     },
-    printType : function(val) {
-        console.log(typeof val);
+    printType : function() {
+        console.log(this.val);
     }
 }
 
+console.log("********************");
 
-let test1 = dynamicType;
-console.log(test1.val);
-test1.put(42);
-console.log(test1.val);
-test1.change("string");
-test1.printType(test1.val);
+let a = dynamicType;
+
+a.put(42); // type === Number
+a.change("Array"); // convert to array with the input;
+a.printType(); // print the converted value on console;
+console.log(typeof a.val); //object 리턴함 (배열은 object의 특수한 형태이기 때문)
+console.log(Array.isArray(a.val)); //Array.isArray(value)로 확인가능 (bool)
+
+console.log("********************");
+
+let b = dynamicType;
+
+b.put(42);
+b.change("String");
+b.printType();
+console.log(typeof b.val);
+
+console.log("********************");
+
+let c = dynamicType;
+
+c.put(-123);
+c.change("Object");
+c.printType();
+console.log(typeof c.val);
+console.log(Array.isArray(c.val));
+
+console.log("********************");
+
+let d = dynamicType;
+
+d.put(42);
+d.change("Number");
+d.printType();
+console.log(typeof d.val);
