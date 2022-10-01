7211c3c5cb8fa5205b2d0f5dc7331f4d45253dfe
Keungeun LEE <klee8903@gmail.com>
2022-10-01 22:49:38 +0200

    js01/ex02 index.html updated

---
+++ js01/ex02/calc.js
@@ -0,0 +1,7 @@
+const productInput = document.querySelectorAll('input')[0];
+// console.log(product);
+const productName = productInput.getAttribute('value');
+console.log(productName);
+
+window.onload = function execute() {
+};
